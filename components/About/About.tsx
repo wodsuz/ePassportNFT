5fe1ba02d1b0879cc3737ddde77ec71d8913f1e0
Ongun <ongun.demirag@gmail.com>
2024-02-04 08:49:25 +0100

    Refactor About module and Carousel styles

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -6,33 +6,36 @@ import { DefaultButton } from "../Buttons";
 
 const About = () => {
   return (
-    <div className={styles.section} id="about">
+    <section className={styles.section} id="about">
       <div className={styles.container}>
         <div className={styles.box}>
           <Carousel />
         </div>
         <div className={styles.box}>
-          <div className={styles.title}>
+          <h2 className={styles.title}>
             Welcome To <br /> ePassport
-          </div>
-          <div className={styles.subText}>
+          </h2>
+          <p className={styles.subText}>
             {" "}
-            The WEIRDOS CLUB is a private collection of NFTs—unique digital
-            collectibles. The Weirdos are stored as ERC-721 tokens on the
-            Ethereum blockchain and hosted on IPFS.
-          </div>
-          <div className={styles.subTextLight}>
-            With more than 200+ hand drawn traits, each NFT is unique and comes
-            with a membership to an exclusive group of successful investors.
-            Join an ambitious ever-growing community with multiple benefits and
-            utilities.
-          </div>
+            Your digital passport to the world of unique, mintable NFTs. Our
+            platform allows you to create and own your digital identity in the
+            form of NFT passports, stored as ERC-721 tokens on the Ethereum
+            blockchain and hosted on IPFS. With ePassport, you're not just
+            minting NFTs, you're crafting your unique digital identity. Step
+            into the future of digital ownership with ePassport.
+          </p>
+          <p className={styles.subTextLight}>
+            Mint your unique NFT passport, own it, trade it, and explore the
+            endless possibilities in the world of digital ownership. Your
+            journey into the future of blockchain technology starts here with
+            ePassport.
+          </p>
           <div className={styles.buttonContainer}>
             <DefaultButton text="JOIN OUR DISCORD" link="#" />
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
