27bcdce6d4aa652c0a7a98dec5e89c10e4d6594a
Ongun <ongun.demirag@gmail.com>
2024-02-07 22:37:09 +0100

    Update Showcase module styling

--- components/Showcase/Showcase.tsx
+++ components/Showcase/Showcase.tsx
@@ -2,17 +2,6 @@ import React, { useRef } from "react";
 import styles from "./Showcase.module.scss";
 import Image from "next/image";
 
-import img1 from "../../assets/Nfts/bighead.svg";
-import img2 from "../../assets/Nfts/bighead-1.svg";
-import img3 from "../../assets/Nfts/bighead-2.svg";
-import img4 from "../../assets/Nfts/bighead-3.svg";
-import img5 from "../../assets/Nfts/bighead-4.svg";
-import img6 from "../../assets/Nfts/bighead-5.svg";
-import img7 from "../../assets/Nfts/bighead-6.svg";
-import img8 from "../../assets/Nfts/bighead-7.svg";
-import img9 from "../../assets/Nfts/bighead-8.svg";
-import img10 from "../../assets/Nfts/bighead-9.svg";
-
 const NftItem = ({
   img,
   number = 0,
