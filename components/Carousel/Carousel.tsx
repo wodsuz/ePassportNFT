284c0ceba3512be761d585588504d2983a7a3da4
Ongun <ongun.demirag@gmail.com>
2024-02-14 06:41:33 +0100

    Update Carousel component and CoverVideo component

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Carousel.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "next/image";
 
 // Import Swiper styles
 import "swiper/css";
@@ -31,34 +32,148 @@ const Carousel = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 1.svg"
+            alt="ePassport swipe first item"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-1.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 2.svg"
+            alt="ePassport swipe second item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-2.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 3.svg"
+            alt="ePassport swipe third item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-3.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 4.svg"
+            alt="ePassport swipe fourth item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-4.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 5.svg"
+            alt="ePassport swipe fifth item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-5.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 6.svg"
+            alt="ePassport swipe sixth item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-6.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 7.svg"
+            alt="ePassport swipe seventh item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-7.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 8.svg"
+            alt="ePassport swipe right item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-8.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 9.svg"
+            alt="ePassport swipe nineth item"
+          />{" "}
         </SwiperSlide>
         <SwiperSlide>
-          <img src="/assets/Nfts/bighead-9.svg" alt="The Weirdos" />
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 10.svg"
+            alt="ePassport swipe tenth item"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 11.svg"
+            alt="ePassport swipe eleventh item"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 12.svg"
+            alt="ePassport swipe twelfth item"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 13.svg"
+            alt="ePassport swipe thirteenth item"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 14.svg"
+            alt="ePassport swipe fourteenth item"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 15.svg"
+            alt="ePassport swipe fifteenth item"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            className={styles.Images}
+            width={100}
+            height={100}
+            src="/assets/Nfts/Group 16.svg"
+            alt="ePassport swipe sixteenth item"
+          />{" "}
         </SwiperSlide>
       </Swiper>
     </div>
--- components/CoverVideo/CoverVideo.tsx
+++ components/CoverVideo/CoverVideo.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./CoverVideo.module.scss";
+import Image from "next/image";
 
 const CoverVideo = () => {
   return (
     <div className={styles.VideoContainer}>
-      <video src="/assets/HomeVideo.mp4" autoPlay muted loop />
+      <Image
+        src="/assets/hpGif.gif"
+        alt="Homepage ePassport gif"
+        height={1000}
+        width={1000}
+      />
     </div>
   );
 };
